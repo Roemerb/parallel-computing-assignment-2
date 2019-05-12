7cdfb72493e7be32351483a3a572ad8a38503580
Roemer Bakker <contact@roemerbakker.com>
2019-05-12 18:06:13 +0200

    - Finished parallel implementation using theadpoolexecutor - Added MergeKArrays function to merge the slices - Added parallel testing method - Changed formatting of test results

--- src/HeapSortParallel.java
+++ src/HeapSortParallel.java
@@ -1,4 +1,9 @@
+import java.util.concurrent.*;
+
 public class HeapSortParallel extends HeapSortSerial {
+
+    private ThreadPoolExecutor exec;
+    private int NUMBER_OF_CORES = Runtime.getRuntime().availableProcessors();
 
     public int[][] sortedChunks;
     public int[] finalSortedArr;
@@ -9,37 +14,45 @@ public class HeapSortParallel extends HeapSortSerial {
         sortedChunks[coreId] = result;
     }
 
-    class SortingThread implements Runnable {
-        int coreId;
-        int[] arr;
-
-        public SortingThread(int coreId, int[] arr)
-        {
-            this.coreId = coreId;
-            this.arr = arr;
-        }
-
-        @Override
-        public void run() {
-            HeapSortSerial hss = new HeapSortSerial();
-            complete(this.coreId, hss.sort(this.arr));
-        }
-    }
-
     @Override
     public int[] sort(int[] arr)
     {
-        int cores = Runtime.getRuntime().availableProcessors();
-        int usableCores = cores-1;
+        sortedChunks = new int[NUMBER_OF_CORES][];
 
-        int[][] divided = Utils.divideArray(arr, usableCores);
-        for (int i = 0; i < usableCores; i++)
+        exec = new ThreadPoolExecutor(
+                NUMBER_OF_CORES * 2,
+                NUMBER_OF_CORES * 2,
+                60L,
+                TimeUnit.SECONDS,
+                new LinkedBlockingQueue<Runnable>()
+        ) {
+            @Override
+            protected void afterExecute(Runnable r, Throwable t) {
+                super.afterExecute(r, t);
+//                System.out.println("Thread has finished!");
+            }
+
+            @Override
+            protected void terminated() {
+                super.terminated();
+//                System.out.println("All threads have finished");
+            }
+        };
+
+
+        int[][] divided = Utils.divideArray(arr, NUMBER_OF_CORES);
+        for (int i = 0; i < NUMBER_OF_CORES; i++)
         {
-            SortingThread st = new SortingThread(i, divided[i]);
-            threads[i] = new Thread(st);
-            threads[i].start();
+            int finalI = i;
+            sortedChunks[finalI] = new int[divided[finalI].length];
+            exec.execute(() -> {
+//                System.out.println("Starting to process thread " + finalI);
+                HeapSortSerial hss = new HeapSortSerial();
+                complete(finalI, hss.sort(divided[finalI]));
+            });
         }
+        exec.shutdown();
 
-        return finalSortedArr;
+        return MergeKArrays.mergeKSortedArray(sortedChunks);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,25 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         System.out.println("Testing serial implementation");
+        System.out.println(String.format("%s\t\t\t%s\t\t%s", "Elements", "Micro S.", "Micro S."));
         testWithNElementsSerial(1000);
         testWithNElementsSerial(10000);
         testWithNElementsSerial(100000);
         testWithNElementsSerial(1000000);
         testWithNElementsSerial(10000000);
 
-        HeapSortParallel hsp = new HeapSortParallel();
-        hsp.sort(Utils.createTestSet(100,100));
+        System.out.println("Testing serial implementation");
+        System.out.println(String.format("%s\t\t\t%s\t\t%s", "Elements", "Micro S.", "Micro S."));
+        testWithNElementsParallel(1000);
+        testWithNElementsParallel(10000);
+        testWithNElementsParallel(100000);
+        testWithNElementsParallel(1000000);
+        testWithNElementsParallel(10000000);
     }
 
     public static void testWithNElementsSerial(int n)
     {
         HeapSortSerial hss = new HeapSortSerial();
-
-        System.out.println("Starting test with " + n + " elements");
 
         int[] arr = Utils.createTestSet(n, Main.MAX_VALUE);
 
@@ -26,6 +30,19 @@ public class Main {
         hss.sort(arr);
         long endTime = System.nanoTime();
         long totalTimeMs = ((endTime - startTime)/1000);
-        System.out.println("Took: " + totalTimeMs + " microseconds (" + (totalTimeMs/1000) + " milliseconds)");
+        System.out.println(String.format("%d\t\t\t%d\t\t\t%d", n, totalTimeMs, (totalTimeMs/1000)));
+    }
+
+    public static void testWithNElementsParallel(int n)
+    {
+        HeapSortParallel hsp = new HeapSortParallel();
+
+        int[] arr = Utils.createTestSet(n, Main.MAX_VALUE);
+
+        long startTime = System.nanoTime();
+        hsp.sort(arr);
+        long endTime = System.nanoTime();
+        long totalTimeMs = ((endTime - startTime)/1000);
+        System.out.println(String.format("%d\t\t\t%d\t\t\t%d", n, totalTimeMs, (totalTimeMs/1000)));
     }
 }
