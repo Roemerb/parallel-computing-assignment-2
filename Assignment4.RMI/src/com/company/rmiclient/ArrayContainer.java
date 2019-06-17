8156bfddc8bad374b126695c626a00dd69c6d068
Amin Chihab <amin_chihab@hotmail.com>
2019-06-17 10:54:53 +0200

    RMI solution

---
+++ Assignment4.RMI/src/com/company/rmiclient/ArrayContainer.java
@@ -0,0 +1,18 @@
+package com.company.rmiclient;
+
+public class ArrayContainer implements Comparable<ArrayContainer>
+{
+    int[] arr;
+    int index;
+
+    public ArrayContainer(int[] arr, int index) {
+        this.arr = arr;
+        this.index = index;
+    }
+
+    @Override
+    public int compareTo(ArrayContainer o) {
+        return this.arr[this.index] - o.arr[o.index];
+    }
+}
+
